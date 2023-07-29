cb85de0aacfce89c971d6154c6b6936bb2a16ef1
NazarStf <hworker98@gmail.com>
2023-07-29 19:09:02 +0300

    Initial Commit

---
+++ A Stroke of Insight/A_Stroke_of_InsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  A_Stroke_of_InsightApp.swift
+//  A Stroke of Insight
+//
+//  Created by NazarStf on 29.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct A_Stroke_of_InsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
